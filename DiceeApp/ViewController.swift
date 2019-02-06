12f96ba6a73e6d11990fb7cac945985f99cc4542
Shalise Ayromloo <shaliseayromloo@shalises-air.hw.ccc.edu>
2019-02-05 20:58:08 -0600

    Created two new variable as well as a random whole number generator that produces random integers between 0 and 5 when the roll button is pressed

--- DiceeApp/ViewController.swift
+++ DiceeApp/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    //create random number genderator and then use that to randomize dice images
 
     @IBOutlet weak var diceImageView1: UIImageView!
     
-    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView! //can't just delete it here--have to righ click on the image and click on the x---OR right click on "diceImageView2" and click on Refactor and then rename
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +26,10 @@ class ViewController: UIViewController {
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         //what should happen when the roll button is pressed
+        randomDiceIndex1 = Int.random(in: 0 ... 5) //create new whole numbers from 0 to 5 (up to and including 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        print(randomDiceIndex1)
     }
     
 }
